47914e459eaa0c14c290a6f535f73344428c30e1
mirelon <miso@github.ksp.sk>
2013-10-27 16:35:31 +0100

    Added secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weaggregather::Application.config.secret_token = '73d9cbddcfbcc2402470d3ce4309ba697fa65c5e72670ad96dfea9bc37fc59d9c5f628cf1ff47a607d07994de7f13ef54ebad4f1b8854f01d0a0945c32b5c8fb'
